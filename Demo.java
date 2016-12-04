162947f7e8e52af7f8258173437c1fdeeb929dbf
YumeZZ <s104213052@mail1.ncnu.edu.tw>
2016-12-04 15:31:59 +0800

    finally complete demo of  random different  numbers ^^

--- Demo.java
+++ Demo.java
@@ -2,23 +2,22 @@ import java.util.Random;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] values = new int[10];
+        int[] values = new int[20];
         Random random = new Random();
-
+        // random different numbers in array
         for(int i = 0; i < values.length; i++){
-            int number = random.nextInt(10) + 1;
-
-            for(int j = 0;j <= i;j++){
-                if(number != values[j]){
-                    values[i]=number;
+            int number = random.nextInt(20) + 1;
+            values[i] = number;
+            for(int j = i - 1; j >= 0; j--) {
+                if(values[i] == values[j]) {
+                    i--;
+                    break;
                 }
             }
         }
-
-        // 遍历数组并打印数据
+        // print result
         for(int i = 0;i < values.length; i++){
             System.out.print(values[i] + "\t");
-
             if(( i + 1 ) % 10 == 0){
                 System.out.println("\n");
             }
