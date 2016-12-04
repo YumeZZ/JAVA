a50ec56de244484eb6bbfbeec1c523c4d0a5e752
YumeZZ <s104213052@mail1.ncnu.edu.tw>
2016-12-04 14:52:58 +0800

    remove some content

--- 九九乘法表.java
+++ 九九乘法表.java
@@ -1,14 +1,4 @@
 public class 九九乘法表 {
-    // public static void main(String[] argv) { // Method Name首字小寫
-    //     // argv: array of references to String object
-    //     int myVariable; // 變數宣告
-    //     int i, sum;
-    //     for (i = 1, sum = 0; i <= 100; i++) {
-    //         sum += i;
-    //     }
-    //     System.out.println("summation from 1 to 100 is "+sum);
-    // }
-
     public static void main(String[] argv) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
